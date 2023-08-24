ce6e404acc527add081b6c2f7046d2c9bc26507d
Sk-Sahil-Islam <sksahilislam0123@gmail.com>
2023-08-24 22:48:54 +0530

    v1.1

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -8,18 +8,26 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Fastfood
 import androidx.compose.material.icons.filled.Help
 import androidx.compose.material.icons.filled.LocalDining
 import androidx.compose.material.icons.filled.LocalFireDepartment
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.WaterDrop
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Fastfood
 import androidx.compose.material.icons.outlined.Help
+import androidx.compose.material.icons.outlined.HelpOutline
 import androidx.compose.material.icons.outlined.LocalDining
 import androidx.compose.material.icons.outlined.LocalFireDepartment
+import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material.icons.outlined.WaterDrop
+import androidx.compose.material.icons.sharp.Menu
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -36,8 +44,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
@@ -45,6 +58,7 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.fitnessapp.ui.navigation_drawer.NavigationItems
 import com.example.fitnessapp.ui.theme.FitnessAppTheme
+import com.example.fitnessapp.ui.theme.Kanit
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -54,97 +68,156 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FitnessAppTheme {
-                val navController = rememberNavController()
-                val navBackStackEntry by navController.currentBackStackEntryAsState()
-                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
-                val scope = rememberCoroutineScope()
-                Surface(
-                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
-                ) {
-                    val items = listOf(
-                        NavigationItems(
-                            route = "food",
-                            title = "Food",
-                            selectedIcon = Icons.Default.LocalDining,
-                            unselectedIcon = Icons.Outlined.LocalDining
-                        ), NavigationItems(
-                            route = "calories",
-                            title = "Calories",
-                            selectedIcon = Icons.Default.LocalFireDepartment,
-                            unselectedIcon = Icons.Outlined.LocalFireDepartment
-                        ), NavigationItems(
-                            route = "water",
-                            title = "Water",
-                            selectedIcon = Icons.Default.WaterDrop,
-                            unselectedIcon = Icons.Outlined.WaterDrop
-                        ),
+                val items = listOf(
+                    NavigationItems(
+                        route = "food",
+                        title = "Food",
+                        selectedIcon = Icons.Default.Fastfood,
+                        unselectedIcon = Icons.Outlined.Fastfood
+                    ),
+                    NavigationItems(
+                        route = "calories",
+                        title = "Calories",
+                        selectedIcon = Icons.Default.LocalFireDepartment,
+                        unselectedIcon = Icons.Outlined.LocalFireDepartment
+                    ),
+                    NavigationItems(
+                        route = "water",
+                        title = "Water",
+                        selectedIcon = Icons.Default.WaterDrop,
+                        unselectedIcon = Icons.Outlined.WaterDrop
+                    ),
 //                        NavigationItems(
 //                            route = "settings",
 //                            title = "Settings",
 //                            selectedIcon = Icons.Default.Settings,
 //                            unselectedIcon = Icons.Outlined.Settings
 //                        ),
-                        NavigationItems(
-                            route = "help",
-                            title = "Help",
-                            selectedIcon = Icons.Default.Help,
-                            unselectedIcon = Icons.Outlined.Help
-                        )
-                    )
+//                    NavigationItems(
+//                        route = "help",
+//                        title = "Help",
+//                        selectedIcon = Icons.Default.Help,
+//                        unselectedIcon = Icons.Outlined.Help
+//                    )
+                )
+                val navController = rememberNavController()
+                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+                var selectedItemIndex by rememberSaveable {
+                    mutableIntStateOf(0)
+                }
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     ModalNavigationDrawer(
+                        gesturesEnabled = false,
                         drawerContent = {
                             ModalDrawerSheet {
-                                Spacer(modifier = Modifier.height(16.dp))
-                                items.forEach { item ->
+                                Spacer(modifier = Modifier.height(6.dp))
+                                IconButton(
+                                    onClick = {
+                                        scope.launch {
+                                            drawerState.close()
+                                        }
+                                    },
+                                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                                ) {
+                                    Icon(
+                                        imageVector = Icons.Default.Close,
+                                        contentDescription = "close"
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(10.dp))
+                                items.forEachIndexed { index, item ->
                                     NavigationDrawerItem(
                                         label = {
                                             Text(text = item.title)
                                         },
-                                        selected = item.route == navBackStackEntry?.destination?.route,
+                                        //selected = item.route == navBackStackEntry?.destination?.route,
+                                        selected = index == selectedItemIndex,
                                         onClick = {
-                                            navController.navigate(item.route) {
-                                                popUpTo(navController.graph.findStartDestination().id)
-                                                launchSingleTop = true
-                                            }
+//                                            navController.navigate(item.route) {
+//                                                popUpTo(navController.graph.findStartDestination().id)
+//                                                launchSingleTop = true
+//                                            }
+                                            selectedItemIndex = index
                                             scope.launch {
                                                 drawerState.close()
                                             }
                                         },
                                         icon = {
                                             Icon(
-                                                imageVector = if (item.route == navBackStackEntry?.destination?.route) {
+                                                imageVector = if (index == selectedItemIndex) {
                                                     item.selectedIcon
                                                 } else item.unselectedIcon,
                                                 contentDescription = item.title
                                             )
                                         },
                                         badge = {
-                                            item.badgeCount?.let{
+                                            item.badgeCount?.let {
                                                 Text(text = item.badgeCount.toString())
                                             }
                                         },
-                                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                                        modifier = Modifier
+                                            .padding(NavigationDrawerItemDefaults.ItemPadding)
                                     )
                                 }
+                                Spacer(modifier = Modifier.weight(1f))
+                                NavigationDrawerItem(
+                                    label = { Text(text = "Help") },
+                                    selected = selectedItemIndex == 3,
+                                    onClick = {
+                                        selectedItemIndex = 3
+                                        scope.launch {
+                                            drawerState.close()
+                                        }
+                                    },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (selectedItemIndex == 3) {
+                                                Icons.Default.Help
+                                            } else Icons.Outlined.HelpOutline,
+                                            contentDescription = "help"
+                                        )
+                                    },
+                                    modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                                )
+                                Spacer(modifier = Modifier.size(16.dp))
                             }
                         },
                         drawerState = drawerState
                     ) {
                         Scaffold(topBar = {
                             TopAppBar(title = {
-                                Text(text = "Step Sync")
-                            }, navigationIcon = {
-                                IconButton(onClick = {
-                                    scope.launch {
-                                        drawerState.open()
+                                Text(
+                                    text = "STEP SYNC",
+                                    fontFamily = Kanit,
+                                    fontWeight = FontWeight.ExtraLight
+                                )
+                            },
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        scope.launch {
+                                            drawerState.open()
+                                        }
+                                    }) {
+                                        Icon(
+                                            imageVector = Icons.Outlined.Menu,
+                                            contentDescription = "menu"
+                                        )
                                     }
-                                }) {
-                                    Icon(
-                                        imageVector = Icons.Default.Menu,
-                                        contentDescription = "menu"
-                                    )
+                                },
+                                actions = {
+                                    IconButton(onClick = {  }) {
+                                        Icon(imageVector = Icons.Outlined.Settings, contentDescription = "settings")
+                                    }
+                                    IconButton(onClick = {  }) {
+                                        Icon(imageVector = Icons.Outlined.AccountCircle, contentDescription = "settings")
+                                    }
                                 }
-                            })
+                            )
                         }) {
 
                         }
--- app/src/main/java/com/example/fitnessapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/fitnessapp/ui/theme/Type.kt
@@ -2,9 +2,16 @@ package com.example.fitnessapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.fitnessapp.R
+
+val Kanit = FontFamily(
+    Font(R.font.kanit_thin, FontWeight.Thin),
+    Font(R.font.kanit_extra_light, FontWeight.ExtraLight)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +21,7 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +38,10 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val ownTypography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = Kanit
+    )
 )
